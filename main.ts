d437456a37bac048c4d8f1b7fcbb77eb812fe7a0
过客 <9714928@qq.com>
2019-01-16 11:14:16 +0800

    tt

--- main.ts
+++ main.ts
@@ -1,7 +1,8 @@
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    # . . . #
-    . # # # .
-    `);
+//% color="#FFAB19"
+namespace sample {
+    //% block
+    //% x.min=-100 x.max=1000
+    export function foo(x: number) {
+        basic.showNumber(0)
+    }
+} 
